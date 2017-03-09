260c3e2af42c2c70f98180d998033606d88b583b
Joaquim Esteves <joaquimbve@hotmail.com>
2017-03-09 00:55:55 +0000

    THIS COULD GO HORRIBLY WRONG

--- Delivery_1/jsonConverter.py
+++ Delivery_1/jsonConverter.py
@@ -11,15 +11,59 @@ import base64
 # log = Logger(debug=settings.DEBUG)
 from pprint import pprint
 
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 con = None
-#sdasd
 
-# with open('data.json') as data_file:    
-    # data = json.load(data_file)
-
-# pprint(data)
+def parseMedication(cur,con,data):
+	if data["resourceType"] != "Medication":
+		print "This is not a medication!"
+		break
+	medID =  "'"+str(data["id"])+"'"
+	text = 'NULL'
+	manufacturer = 'NULL'
+	isBrand = 'NULL'
+	code = 'NULL'
+	display = 'NULL'
+	system = 'NULL'
+	if 'text' in data :
+		#ALWAYS ENCODE PLAIN TEXT WHEN DEALING WITH JSONS
+		#JSONS ARE REALLY BLOODY FINNICKY
+		text = str(data["text"])
+		encodedText = "'"+str(base64.b64encode(text))+"'"
+	if 'manufacturer' in data:
+		manufacturer = "'"+str(data['manufacturer']["reference"])+"'"
+	if 'isBrand' in data:
+		isBrand = "'"+str(data['isBrand'])+"'"
+	if 'code' in data:
+		#For some SILLY REASON, code also apparently can have text!
+		if 'text' in data['code']:
+			text = base64.b64decode(encodedText) + "Coding Text: " + str(data['code']['text'])
+			encodedText = "'"+str(base64.b64encode(text))+"'"
+		# coding = "'"+str(data['code']['coding'])+"'"
+		if 'coding' in data['code']:
+			code = "'["
+			display = "'["
+			system = "'["
+			max_items = len(data['code']['coding'])
+			i = 0
+			while i < max_items:
+				print i
+				code += str(data['code']['coding'][i]['code'])+","
+				display += str(data['code']['coding'][i]['display'])+","
+				system += str(data['code']['coding'][i]['system'])+","
+				i += 1
+			code += "]'"
+			display += "]'"
+			system += "]'"
+	medicationInsert = "INSERT INTO Medication VALUES ("+medID+""
+	medicationInsert +=	","+encodedText+","+manufacturer+","+isBrand+","+code+","+display+","+system+");"
+	# print "HERE IT COMES\n\n"
+	# print medicationInsert
+	
+	cur.execute(medicationInsert)
+	con.commit();	
+	cur.execute("SELECT * FROM Medication;")
+	print "Printing out the complete list of Medications!"
+	print cur.fetchall()
 
 
 if __name__ == "__main__":
@@ -42,21 +86,6 @@ if __name__ == "__main__":
 		
 	print "Sucessfully connected to db!"
 	
-	
-	###### MONTERS BELLOW
-	"""
-	# format of command is ./user [-n TCSname] [-p TCSport] so get those arguments and validate them
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-n', dest='tcs_name', type=str, default=settings.DEFAULT_TCS_NAME,
-						help='Translation Contact Server IP Address.')
-	parser.add_argument('-p', dest='tcs_port', type=int, default=settings.DEFAULT_TCS_PORT,
-						help='Translation Contact Server Port Address.')
-	args = parser.parse_args()	# validate them
-	# print information just to make sure
-	log.debug("Using TCS Name = {}, TCS Port = {}.".format(args.tcs_name, args.tcs_port))
-	log.info("Welcome :).")
-	# forever & ever (util "exit")
-	"""
 	try:
 		while(True):
 			# waits for client input:
@@ -65,56 +94,7 @@ if __name__ == "__main__":
 			with open(input_data) as data_file:    
 				data = json.load(data_file)
 				try:
-					if data["resourceType"] != "Medication":
-						print "This is not a medication!"
-						break
-					medID =  "'"+str(data["id"])+"'"
-					text = 'NULL'
-					manufacturer = 'NULL'
-					isBrand = 'NULL'
-					code = 'NULL'
-					display = 'NULL'
-					system = 'NULL'
-					if 'text' in data :
-						#ALWAYS ENCODE PLAIN TEXT WHEN DEALING WITH JSONS
-						#JSONS ARE REALLY BLOODY FINNICKY
-						text = str(data["text"])
-						encodedText = "'"+str(base64.b64encode(text))+"'"
-					if 'manufacturer' in data:
-						manufacturer = "'"+str(data['manufacturer']["reference"])+"'"
-					if 'isBrand' in data:
-						isBrand = "'"+str(data['isBrand'])+"'"
-					if 'code' in data:
-						#For some SILLY REASON, code also apparently can have text!
-						if 'text' in data['code']:
-							text = base64.b64decode(encodedText) + "Coding Text: " + str(data['code']['text'])
-							encodedText = "'"+str(base64.b64encode(text))+"'"
-						# coding = "'"+str(data['code']['coding'])+"'"
-						if 'coding' in data['code']:
-							code = "'["
-							display = "'["
-							system = "'["
-							max_items = len(data['code']['coding'])
-							i = 0
-							while i < max_items:
-								print i
-								code += str(data['code']['coding'][i]['code'])+","
-								display += str(data['code']['coding'][i]['display'])+","
-								system += str(data['code']['coding'][i]['system'])+","
-								i += 1
-							code += "]'"
-							display += "]'"
-							system += "]'"
-					medicationInsert = "INSERT INTO Medication VALUES ("+medID+""
-					medicationInsert +=	","+encodedText+","+manufacturer+","+isBrand+","+code+","+display+","+system+");"
-					# print "HERE IT COMES\n\n"
-					# print medicationInsert
-					
-					cur.execute(medicationInsert)
-					con.commit();	
-					cur.execute("SELECT * FROM Medication;")
-					print "Printing out the complete list of Medications!"
-					print cur.fetchall()
+					parseMedication(cur,con,data)
 				except ValueError:
 					print "Error %s:" % e.args[0]
 					break
